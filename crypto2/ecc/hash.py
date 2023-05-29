f79a925bb4151f8e85ddbb4c0de802475bb4e01f
Jacob Larsen <jacobm@larsen.net>
2023-05-29 02:20:51 -0600

    Corrected Galois functions and solved various matrices defined and inverses calculated

--- crypto2/ecc/hash.py
+++ crypto2/ecc/hash.py
@@ -7,6 +7,7 @@ import base64
 import mpmath
 from crypto2.utils.subs import s_box
 from crypto2.ecc.ecc import Curve
+import crypto2.utils.matrices as matrices
 
 
 class Hasher:
@@ -15,24 +16,24 @@ class Hasher:
         self.__r = bytearray(8)
         self.ecc = Curve()
 
-        # I use an initialization vector for this hash algorith of pi, built to 512 bits
-        # I chose this because
-        mpmath.mp.dps = 512  # Set the desired precision in decimal places
-        pi = mpmath.mp.pi  # Compute pi with the specified precision
-        # convert pi to 512 bit int... remember 3 in 3.13 is 2 bits thus 512-2 = 510
-
-        self.__internal = bytearray(int(pi * 2 ** 510).to_bytes(length=64, byteorder='big'))
+        # I used sqrt of 2 as the initialization vector because it is a well known irrational number
+        # I did not use pi because it is used as part of my one-way function (more below)
+        self.__internal = matrices.def_root2rix()
 
         if b64_seed:
             self.__seed = base64.b64decode(b64_seed)
 
-    def hash(self, _64_bytes, is_b64_encoded=False):
+    def hash(self, _64_bytes=None, is_b64_encoded=False):
+        # If no additional information is provided, then we just want to generate some data and hash again
+        # I choose sqrt 3 since it is a well-known irrational number that shouldn't be related to pi in any way
         if not _64_bytes:
-            _64_bytes = self.__internal
+            _64_bytes = matrices.def_inv_root3rix()
 
+        # Decode input as a bytearray if needed
         if is_b64_encoded:
             _64_bytes = bytearray(base64.b64decode(_64_bytes))
 
+        # Pad data with 0's if needed
         if len(_64_bytes) < 64:
             for _ in range(64 - len(_64_bytes)):
                 _64_bytes.append(0x00)
@@ -40,6 +41,7 @@ class Hasher:
             for _ in range(64 - (len(_64_bytes) % 64)):
                 _64_bytes.append(0x00)
 
+        # break data into blocks
         for j in range(len(_64_bytes) // 64):
             for i in range(64):
                 # This xors the internal and input byte matrices, puts the output through the aes substitution box,
@@ -66,10 +68,10 @@ class Hasher:
         _64_bytes = _64_bytes[64:]
 
     def digest(self, as_b64=False):
-        return self.__internal if not as_b64 else base64.b64encode(self.__internal)
+        return bytearray(self.__internal) if not as_b64 else base64.b64encode(bytearray(self.__internal))
 
 
-"""
+
 h = Hasher()
 print(h.digest(as_b64=True))
 
@@ -78,4 +80,4 @@ for _ in range(1):
     b64 = base64.b64encode(b"this is a really long bit of information that is designed to make it so that the entire thing is just super long. In this code, you can replace process_chunk with the specific function or operations you need to perform on each 64-byte chunk. You can modify the buffer size (64 in this example) based on your requirements.In this code, you can replace process_chunk with the specific function or operations you need to perform on each 64-byte chunk. You can modify the buffer size (64 in this example) based on your requirements.In this code, you can replace process_chunk with the specific function or operations you need to perform on each 64-byte chunk. You can modify the buffer size (64 in this example) based on your requirements.")
     h.hash(None, is_b64_encoded=False)
     print(h.digest(as_b64=True))
-"""
+
---
+++ crypto2/utils/matrices.py
@@ -0,0 +1,136 @@
+from crypto2.utils.galois import galois_multiply, galois_inverse
+
+
+# sooooooo pi has no inverse.... this is super important later... but it doesn't work for this
+""" Here are the values as I attempt to inverse the matrices:
+201	196	41	2	81	239	48	79	
+15	198	2	11	74	149	43	225	
+218	98	78	190	8	25	10	53	
+162	139	8	166	121	179	109	109	
+33	128	138	59	142	205	242	109	
+104	220	103	19	52	58	95	81	
+194	28	204	155	4	67	20	194	
+52	209	116	34	221	27	55	69	
+
+122	157	224	238	206	83	2	137	
+0	0	0	0	0	0	0	0	
+140	125	78	140	242	6	87	27	
+229	63	65	109	220	163	160	196	
+75	105	167	183	77	118	39	103	
+99	141	1	144	174	158	127	56	
+149	178	44	238	175	33	154	39	
+2	124	253	10	23	191	51	119	
+
+96	0	181	99	65	221	36	37	
+31	0	135	128	29	93	95	12	
+48	0	212	191	174	224	18	156	
+158	0	48	6	249	160	214	195	
+101	0	194	198	19	248	34	130	
+100	0	205	51	77	108	120	213	
+168	0	10	182	32	180	139	80	
+54	0	71	217	179	2	183	123	
+
+0	0	0	0	0	0	0	0	
+0	0	0	0	0	0	0	0	
+0	0	0	0	0	0	0	0	
+0	0	0	0	0	0	0	0	
+0	0	0	0	0	0	0	0	
+0	0	0	0	0	0	0	0	
+0	0	0	0	0	0	0	0	
+0	0	0	0	0	0	0	0	
+# By 4th matrix, pi has reduced to an irreducible  matrix
+"""
+
+
+def def_pitrix():
+    # This is pi with a precision of 512 bits generated like
+    # mpmath.mp.dps = 512
+    # pi = mpmath.mp.pi
+    # piis = bytearray(int(pi * 2 ** 510).to_bytes(length=64, byteorder='big'))
+    # I then broke the bytearray into a 2d array of the first 64 bytes
+    # This matrix has no inverse, see note above
+    return [
+        201, 196, 41, 2, 81, 239, 48, 79,
+        15, 198, 2, 11, 74, 149, 43, 225,
+        218, 98, 78, 190, 8, 25, 10, 53,
+        162, 139, 8, 166, 121, 179, 109, 109,
+        33, 128, 138, 59, 142, 205, 242, 109,
+        104, 220, 103, 19, 52, 58, 95, 81,
+        194, 28, 204, 155, 4, 67, 20, 194,
+        52, 209, 116, 34, 221, 27, 55, 69
+    ]
+
+
+def def_root2rix():
+    # sqrt_two = mpmath.mp.sqrt(2)
+    # sqrt_two_bits = bytearray(int(sqrt_two * 2 ** 511).to_bytes(length=64, byteorder='big'))
+    return [
+        181, 89, 29, 237, 74, 168, 15, 120,
+        4, 125, 111, 23, 252, 177, 116, 4,
+        243, 137, 96, 172, 131, 254, 168, 135,
+        51, 179, 186, 133, 4, 111, 94, 54,
+        249, 117, 137, 131, 58, 220, 67, 61,
+        222, 74, 59, 51, 184, 131, 156, 250,
+        100, 190, 168, 153, 162, 219, 123, 39,
+        132, 159, 76, 21, 195, 57, 74, 104
+    ]
+
+
+def def_inv_root2rix():
+    return [
+        87, 194, 111, 169, 200, 38, 195, 160,
+        133, 246, 115, 224, 69, 75, 164, 97,
+        155, 206, 56, 19, 16, 247, 147, 165,
+        24, 60, 164, 231, 234, 158, 195, 147,
+        149, 240, 53, 0, 197, 233, 113, 150,
+        28, 21, 24, 67, 69, 88, 150, 99,
+        167, 156, 77, 74, 207, 170, 136, 3,
+        78, 70, 195, 198, 174, 97, 235, 255
+    ]
+
+def def_root3rix():
+    # sqrt_three = mpmath.mp.sqrt(3)
+    # sqrt_three_bits = bytearray(int(sqrt_three * 2 ** 511).to_bytes(length=64, byteorder='big'))
+    return [
+        221, 146, 146, 99, 36, 212, 230, 104,
+        179, 186, 236, 144, 133, 201, 38, 102,
+        215, 22, 26, 83, 231, 133, 121, 153,
+        66, 184, 102, 36, 236, 87, 38, 208,
+        194, 60, 41, 55, 175, 9, 209, 13,
+        101, 92, 237, 34, 120, 17, 208, 108,
+        83, 29, 35, 211, 174, 71, 246, 209,
+        157, 196, 204, 113, 222, 195, 52, 193
+    ]
+
+def def_inv_root3rix():
+    return [
+        127, 125, 125, 252, 78, 184, 127, 19,
+        246, 24, 113, 97, 244, 241, 45, 175,
+        102, 53, 102, 140, 97, 15, 112, 42,
+        52, 211, 198, 89, 183, 72, 121, 220,
+        146, 211, 198, 7, 119, 80, 251, 166,
+        81, 182, 139, 53, 145, 138, 240, 190,
+        42, 179, 228, 199, 63, 191, 123, 136,
+        87, 170, 152, 30, 80, 220, 90, 118,
+    ]
+
+def print_matrix(e, size=8):
+    s = ""
+
+    for j in range(size):
+        for l in range(size):
+            s += f"{e[j * size + l]}\t"
+        s += "\n"
+
+    print(s)
+
+
+def mult_matrix(a, b, size=8):
+    r = bytearray(size * size)  # Initialize result bytearray with zeros
+
+    for i in range(size):  # for each row
+        for j in range(size):
+            for k in range(size):
+                r[i*size + j] ^= galois_multiply(a[i*size + k], b[k*size + j])
+
+    return r
